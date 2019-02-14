4af4c7dd613c04df9489ff2aab597e97a9f1a9a1
Satori <comfycallum@gmail.com>
2019-02-14 21:16:34 +0000

    Start implementing main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
 
 fn main() {
+    let config = load_yaml!("../config.yml");
+    let matches = App::from_yaml(config).get_matches();
 
+    let binary = matches.value_of("binary").unwrap();
+    let metadata = matches.value_of("metadata").unwrap();
+
+    println!("binary: {} metadata: {}", binary, metadata);
 }
